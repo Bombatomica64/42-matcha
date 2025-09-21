cc76e2ad5fd504a708297ba96322410e16f46595
marcor2742 <marcoruggiero.mail@gmail.com>
2025-09-22 00:04:52 +0200

    tinder cards animate

--- Front-End/src/app/components/card/card.ts
+++ Front-End/src/app/components/card/card.ts
@@ -1,33 +1,68 @@
 import { Component, input } from "@angular/core";
 import { ButtonModule } from "primeng/button";
 import { CardModule } from "primeng/card";
+import { TagModule } from 'primeng/tag';
 import type { components } from "../../../types/api";
 
 type User = components["schemas"]["User"];
 
 @Component({
 	selector: "app-card",
-	imports: [CardModule, ButtonModule],
+	imports: [CardModule, ButtonModule, TagModule],
 	template: `
-    <p-card [style]="{ width: '25rem', overflow: 'hidden', height: '100%' }">
-      <ng-template #header>
-          <img alt="Foto profilo" class="w-full" [src]="getMainPhotoUrl()" />
-      </ng-template>
-      <ng-template #title>{{ user().first_name }} {{ user().last_name }}</ng-template>
-      <ng-template #subtitle>Età: {{ calculateAge(user().birth_date) }}</ng-template>
-      <p class="bio">
-        {{ user().bio || 'Nessuna bio disponibile' }}
-      </p>
-      <ng-template #footer>
-          <div class="flex gap-4 mt-1">
-              <p-button label="Cancel" severity="secondary" class="w-full" [outlined]="true" styleClass="w-full" />
-              <p-button label="Save" class="w-full" styleClass="w-full" />
-          </div>
-      </ng-template>
-    </p-card>
+      <img alt="Foto profilo" class="w-full" [src]="getMainPhotoUrl()" />
+
+      <div class="card-body">
+        <div class="info">
+          <p-tag [style]="{ border: '2px solid var(--border-color)', background: 'transparent', color: 'var(--text-color)'}">
+              <div class="flex items-center gap-2 px-1">
+                  <span class="text-base">
+                      {{ user().first_name }} {{ user().last_name }}
+                  </span>
+              </div>
+          </p-tag>
+
+          <p class="bio">
+          bio: <br>
+          {{ user().bio || 'Nessuna bio disponibile' }}
+          </p>
+          <p><br>{{ calculateAge(user().birth_date) }} anni</p>
+        </div>
+
+        <div class="card-footer">
+            <p-button label="Cancel" severity="secondary" class="w-full" [outlined]="true" styleClass="w-full" />
+            <p-button label="Save" class="w-full" styleClass="w-full" />
+        </div>
+      </div>
+
   `,
 	styles: `
-
+  img {
+    border-radius: 8px;
+  }
+  :host {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    height: 100%;
+    z-index: 20 !important;
+    background: #383737ff;
+    border-radius: 8px;
+  }
+  .card-body {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    padding: 1rem;
+  }
+  .card-footer {
+    display: flex;
+    gap: 0.75rem;
+    align-items: center;
+    justify-content: space-between;
+    flex-shrink: 0;
+  }
   `,
 })
 export class Card {
